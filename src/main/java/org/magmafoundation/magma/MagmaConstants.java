340d5097905767012245a11583815be285079f0e
Keir Davis <minecraftfun201@gmail.com>
2022-06-20 19:12:47 +0100

    Fixed Loading

--- src/main/java/org/magmafoundation/magma/MagmaConstants.java
+++ src/main/java/org/magmafoundation/magma/MagmaConstants.java
@@ -10,7 +10,7 @@ public class MagmaConstants {
 
     public static final String NAME = "Magma";
     public static final String BRAND = "MagmaFoundation";
-    public static final String VERSION = !Objects.equals(fullVersion, "dev-env") ? fullVersion.split("-")[0] + "-" + fullVersion.split("-")[2] : "dev-env";
+    public static final String VERSION = !Objects.equals(fullVersion, "dev-env") ? fullVersion.split("-")[0] + "-" + fullVersion.split("-")[1] : "dev-env";
     public static final String BUKKIT_VERSION = "v1_12_R1";
     public static final String FORGE_VERSION_FULL = fullVersion;
     public static final String FORGE_VERSION = fullVersion.substring(0, fullVersion.lastIndexOf("-"));
