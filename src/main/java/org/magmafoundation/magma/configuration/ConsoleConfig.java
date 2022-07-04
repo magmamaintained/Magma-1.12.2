cf034e4821b67f913c17d567e5b9199b1a223d12
UeberallGebannt <paulmertens63@gmail.com>
2022-07-04 16:08:19 +0200

    Refactor the configs + Fixes

--- src/main/java/org/magmafoundation/magma/configuration/ConsoleConfig.java
+++ src/main/java/org/magmafoundation/magma/configuration/ConsoleConfig.java
@@ -19,6 +19,7 @@ import java.util.logging.Level;
 public class ConsoleConfig extends ConfigBase {
 
     public static ConsoleConfig instance = new ConsoleConfig();
+    public static final int CONFIG_VERSION = 1;
 
     public ConsoleConfig() {
         super("console.yml", "console");
@@ -27,30 +28,27 @@ public class ConsoleConfig extends ConfigBase {
 
     //============================Console======================================
 
-    public final StringValue highlightLevelError = new StringValue(this, "console.colour.level.error", "c", "The colour for the error level");
-    public final StringValue highlightLevelWarning = new StringValue(this, "console.colour.level.warning", "e", "The colour for the warning level");
-    public final StringValue highlightLevelInfo = new StringValue(this, "console.colour.level.info", "2", "The colour for the info level");
-    public final StringValue highlightLevelFatal = new StringValue(this, "console.colour.level.fatal", "e", "The colour for the fatal level");
-    public final StringValue highlightLevelTrace = new StringValue(this, "console.colour.level.trace", "e", "The colour for the trace level");
+    public final StringValue highlightLevelError = new StringValue(this, "console.color.level.error", "c", "The color for the error level");
+    public final StringValue highlightLevelWarning = new StringValue(this, "console.color.level.warning", "e", "The color for the warning level");
+    public final StringValue highlightLevelInfo = new StringValue(this, "console.color.level.info", "2", "The color for the info level");
+    public final StringValue highlightLevelFatal = new StringValue(this, "console.color.level.fatal", "e", "The color for the fatal level");
+    public final StringValue highlightLevelTrace = new StringValue(this, "console.color.level.trace", "e", "The color for the trace level");
 
-    public final StringValue highlightMessageError = new StringValue(this, "console.colour.message.error", "c", "The colour for the error message");
-    public final StringValue highlightMessageWarning = new StringValue(this, "console.colour.message.warning", "e", "The colour for the warning message");
-    public final StringValue highlightMessageInfo = new StringValue(this, "console.colour.message.info", "2", "The colour for the info message");
-    public final StringValue highlightMessageFatal = new StringValue(this, "console.colour.message.fatal", "e", "The colour for the fatal message");
-    public final StringValue highlightMessageTrace = new StringValue(this, "console.colour.message.trace", "e", "The colour for the trace message");
+    public final StringValue highlightMessageError = new StringValue(this, "console.color.message.error", "c", "The color for the error message");
+    public final StringValue highlightMessageWarning = new StringValue(this, "console.color.message.warning", "e", "The color for the warning message");
+    public final StringValue highlightMessageInfo = new StringValue(this, "console.color.message.info", "2", "The color for the info message");
+    public final StringValue highlightMessageFatal = new StringValue(this, "console.color.message.fatal", "e", "The color for the fatal message");
+    public final StringValue highlightMessageTrace = new StringValue(this, "console.color.message.trace", "e", "The color for the trace message");
 
-    public final StringValue highlightTimeError = new StringValue(this, "console.colour.time.error", "c", "The colour for the error time");
-    public final StringValue highlightTimeWarning = new StringValue(this, "console.colour.time.warning", "e", "The colour for the warning time");
-    public final StringValue highlightTimeInfo = new StringValue(this, "console.colour.time.info", "2", "The colour for the info time");
-    public final StringValue highlightTimeFatal = new StringValue(this, "console.colour.time.fatal", "e", "The colour for the fatal time");
-    public final StringValue highlightTimeTrace = new StringValue(this, "console.colour.time.trace", "e", "The colour for the trace time");
-
+    public final StringValue highlightTimeError = new StringValue(this, "console.color.time.error", "c", "The color for the error time");
+    public final StringValue highlightTimeWarning = new StringValue(this, "console.color.time.warning", "e", "The color for the warning time");
+    public final StringValue highlightTimeInfo = new StringValue(this, "console.color.time.info", "2", "The color for the info time");
+    public final StringValue highlightTimeFatal = new StringValue(this, "console.color.time.fatal", "e", "The color for the fatal time");
+    public final StringValue highlightTimeTrace = new StringValue(this, "console.color.time.trace", "e", "The color for the trace time");
 
     public ConsoleConfig(String fileName, String commandName) {
         super(fileName, commandName);
     }
-
-
 
     public static String getString(String s, String key, String defaultreturn) {
         if (s.contains(key)) {
@@ -112,8 +110,8 @@ public class ConsoleConfig extends ConfigBase {
                 config.addDefault(toggle.path, toggle.key);
                 values.get(toggle.path).setValues(config.getString(toggle.path));
             }
-            version = getInt("config-version", 1);
-            set("config-version", 1);
+            version = getInt("config-version", CONFIG_VERSION);
+            set("config-version", CONFIG_VERSION);
 
 
             config.options().header(header.toString());
